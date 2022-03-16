c8b78d3071c418be19a0a57374364f989c9abae1
Isaac Shafer <izakshafer@gmail.com>
2022-03-16 12:17:25 -0600

    added css

--- server/server.js
+++ server/server.js
@@ -7,9 +7,11 @@ app.use(express.json())
 
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
-
+app.get('/styles', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.css'))
+})
 const port = process.env.PORT || 4005
 
 app.listen(port, () => console.log('running on 4005'))
